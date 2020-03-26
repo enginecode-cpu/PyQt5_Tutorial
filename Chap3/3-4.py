9674755428f7238b07ef358aaa286c35cbe07a50
enginecode-cpu <bassmatics1002@gmail.com>
2020-03-26 20:22:35 +0900

    add comment

--- Chap3/3-4.py
+++ Chap3/3-4.py
@@ -10,10 +10,10 @@ class MyApp(QWidget):
         self.initUI()
 
     def initUI(self):
-        cb = QCheckBox("Show Title", self)
+        cb = QCheckBox("Show Title", self) # Show Title 이라는 체크 박스를 하나 만든다.
         cb.move(20, 20)
-        cb.toggle()
-        cb.stateChanged.connect(self.changeTitle)
+        cb.toggle() # default가 체크가 해제된 상태로 나타나서 바꿔주기 위해서 사용한다.
+        cb.stateChanged.connect(self.changeTitle) # 체크 박스의 상태가 바뀔 때 발생하는 시그널을 changeTitle과 연결한다.
 
         self.setWindowTitle("QCheckBox")
         self.setGeometry(300, 300, 400, 200)
